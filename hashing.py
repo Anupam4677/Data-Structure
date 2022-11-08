071c962bf8f79b4736ae955f92785fe0b38b2f38
Anupam Prakash <anupamprakash01@gmail.com>
2022-11-08 22:58:17 +0530

    Hash Table
    Hash Table without collision

--- hashing.py
+++ hashing.py
@@ -19,18 +19,34 @@ class HashTable:
         for char in key:
             h += ord(char)
         return h % self.Max
-    def add(self,key,val):
+    # def add(self,key,val):
+    def __setitem__(self,key,val):
         # convert the key into a hash value 
         h = get_hash(key)
         # Now the hash valueis the new key 
         self.arr[h] = val
-    def get(self,key):
+    def __getitem__(self,key):
         h_index = self.get_hash(key)
         return self.arr[h_index]
+    def __delitem__(self,key):
+        h = get_hash(key)
+        self.arr[h] = None
+
 
 t = HashTable()
-t.add('march 16',130)
-t.add('march 6',10)
-print(t.get('march 6'))
+# t.add('march 16',130)
+# t.add('march 6',10)
+# print(t.get('march 6'))
 # print(t.arr)
 # print(t.get_hash('march 28'))
+
+t['june 3'] = 12
+t['april 12'] = 90
+t['jan 26'] = 72
+t['feb 14'] = 54
+t['march 5'] = 89
+
+del t['feb 14']
+print(t['june 3'])
+print(t['feb 14'])
+print(t.arr)
